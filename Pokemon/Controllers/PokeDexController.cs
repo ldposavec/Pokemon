8adb188043a63e38e36c90a497920cbdd40fba67
office10 <office10@ucione.local>
2024-05-27 10:18:41 +0200

    Initial commit

---
+++ Pokemon/Controllers/PokeDexController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Pokemon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private static readonly List<PokeDex> _pokemonList = new List<PokeDex>
+        {
+            new PokeDex { Id = 1, Name = "Bulbasaur", Type = PokeType.Grass },
+            new PokeDex { Id = 2, Name = "Charmander", Type = PokeType.Fire },
+            new PokeDex { Id = 3, Name = "Squirtle", Type = PokeType.Water }
+            // Add more Pokémon here...
+        };
+
+        [HttpGet("{id}")]
+        public ActionResult<PokeDex> GetPokemonById(int id)
+        {
+            var pokemon = _pokemonList.Find(p => p.Id == id);
+            if (pokemon == null)
+            {
+                return NotFound("Pokemon not found");
+            }
+
+            return Ok(pokemon);
+        }
+
+        [HttpPost]
+        public ActionResult<PokeDex> CreatePokemon(PokeDex newPokemon)
+        {
+            // You can add validation logic here (e.g., check if the ID is unique)
+            _pokemonList.Add(newPokemon);
+            return CreatedAtAction(nameof(GetPokemonById), new { id = newPokemon.Id }, newPokemon);
+        }
+    }
+}
---
+++ Pokemon/PokeDex.cs
@@ -0,0 +1,17 @@
+﻿namespace Pokemon
+{
+    public class PokeDex
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public PokeType Type { get; set; }
+        public int Level { get; set; }
+    }
+
+    public enum PokeType
+    {
+        Grass = 0,
+        Fire = 1,
+        Water = 2
+    }
+}
